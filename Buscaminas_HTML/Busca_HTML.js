51e2b4f97e78d5f7f38e90fe8ec3321c64973b22
ANDERAAM <48502025+ANDERAAM@users.noreply.github.com>
2020-05-03 17:48:12 -0500

    Versión 3.0

--- Buscaminas_HTML/Busca_HTML.js
+++ Buscaminas_HTML/Busca_HTML.js
@@ -1,19 +1,27 @@
 var PClick = false;
 var Img    = new Array();
-//    aleatorio=Math.round(Math.random()*(16-1)+parseInt(1));
+
 Img[0]="0.png";Img[1]="1.png";Img[2]="4.png";
-Img[3]="3.png";Img[4]="Mina.png";
+Img[3]="3.png";Img[4]="Mina.png";var Con=0;
 
 
 function imgC(imagen){
-        aleatorio=Math.round(Math.random()*(4-0)+parseInt(0));
-        imagen.src=Img[aleatorio];
-        imagen.onclick=function(){
-        //var Padre = document.getElementById("AA1");
-        alert(imagen.id);
-        //var Frente  = document.getElementById("img");
-        //Padre.parentNode.removeChild(Padre);
-         // Remove <ul>'s first child node (index 0)
-      //  Padre.removeChild(Frente.items(0));
-        }
+  Con++;
+  if(Con<=10){
+  var aleatorio=aleatorio=Math.round(Math.random()*(4-0)+parseInt(0));
+  imagen.src=Img[aleatorio];
+  imagen.onclick=function(){}
+  }else{
+  alert("Has_Perdido");
+  var aleatorio=aleatorio=Math.round(Math.random()*(4-0)+parseInt(0));
+  imagen.src=Img[4];
+  imagen.onclick=function(){}
+  }
+
+}
+
+function Game_O(){
+  var AvisoG = document.createElement("div");
+  
+
 }
