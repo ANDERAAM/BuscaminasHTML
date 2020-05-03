48100e78be7a07f7f16690c98e0e832992da8971
ANDERAAM <48502025+ANDERAAM@users.noreply.github.com>
2020-05-03 14:41:00 -0500

    Version 3.0

--- Buscaminas_HTML/Busca_HTML.js
+++ Buscaminas_HTML/Busca_HTML.js
@@ -1,9 +1,19 @@
-
-var Img = new Array();
+var PClick = false;
+var Img    = new Array();
 //    aleatorio=Math.round(Math.random()*(16-1)+parseInt(1));
+Img[0]="0.png";Img[1]="1.png";Img[2]="4.png";
+Img[3]="3.png";Img[4]="Mina.png";
+
+
 function imgC(imagen){
-
- for(var i=0;i<)
-
-  alert("Hola_Mundo!!! : "+imagen.id);
+        aleatorio=Math.round(Math.random()*(4-0)+parseInt(0));
+        imagen.src=Img[aleatorio];
+        imagen.onclick=function(){
+        //var Padre = document.getElementById("AA1");
+        alert(imagen.id);
+        //var Frente  = document.getElementById("img");
+        //Padre.parentNode.removeChild(Padre);
+         // Remove <ul>'s first child node (index 0)
+      //  Padre.removeChild(Frente.items(0));
+        }
 }
